7b46b458c1e3ed63cf36a23c33edc9b28ea781fc
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2025-06-06 18:46:10 +0530

    added cursor changes and login/signup pages

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', is_home=True)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    # if request.method == 'POST':
+    #     # handle form data
+    #     name = request.form['name']
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     # add logic (e.g., save to DB)
+    #     return "Signed up successfully!"
+    return render_template('signup.html', is_home=False)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', is_home=False)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
